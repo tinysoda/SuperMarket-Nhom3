cdab8c4f95ba8811c3e231329099909fac8fa690
Valiant <rainguy.shy@gmail.com>
2024-07-09 01:01:27 +0700

    init login view

--- SuperMarket/src/main/java/com/java/supermarket/LoginController.java
+++ SuperMarket/src/main/java/com/java/supermarket/LoginController.java
@@ -3,7 +3,7 @@ package com.java.supermarket;
 import java.io.IOException;
 import javafx.fxml.FXML;
 
-public class PrimaryController {
+public class LoginController {
 
     @FXML
     private void switchToSecondary() throws IOException {
